db699e6ad5ea9c613fc54c3613f1f31539ca101e
Pascal Knecht <pascal@addere.ch>
2024-04-12 14:26:31 +0200

    Use Gradle dependency lock files for all projects

--- buildSrc/src/main/kotlin/vulnlog.common-convention.gradle.kts
+++ buildSrc/src/main/kotlin/vulnlog.common-convention.gradle.kts
@@ -23,3 +23,19 @@ tasks.test {
 kotlin {
     jvmToolchain(17)
 }
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+tasks.register("resolveAndLockAll") {
+    notCompatibleWithConfigurationCache("Filters configurations at execution time")
+    doFirst {
+        require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
+    }
+    doLast {
+        configurations.filter {
+            it.isCanBeResolved
+        }.forEach { it.resolve() }
+    }
+}
