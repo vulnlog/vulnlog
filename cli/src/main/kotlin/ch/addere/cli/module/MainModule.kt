25675ea236dfd7b1a8de77c720ffcc5198b62aca
Pascal Knecht <pascal@addere.ch>
2024-06-02 20:22:50 +0200

    Fix missing interface bind in dependency injection module

--- cli/src/main/kotlin/ch/addere/cli/module/MainModule.kt
+++ cli/src/main/kotlin/ch/addere/cli/module/MainModule.kt
@@ -1,10 +1,12 @@
 package ch.addere.cli.module
 
+import ch.addere.cli.command.Service
 import ch.addere.cli.command.ServiceImpl
+import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val mainModule =
     module {
-        singleOf(::ServiceImpl)
+        singleOf(::ServiceImpl) { bind<Service>() }
     }
