903e9e7da7520424a1d635717ab8c610d95acc89
Pascal Knecht <pascal@addere.ch>
2024-06-16 21:03:07 +0200

    Dependencies: Update ktlint and suppress vulnerabilities

--- vl.vulnlog.kts
+++ vl.vulnlog.kts
@@ -5,20 +5,40 @@ val r1 = branch("release 1.0", v100, v010)
 
 cve("CVE-2023-6378") {
     snyk("SNYK-JAVA-CHQOSLOGBACK-6094942", v010)
+    suppress(
+        v010,
+        rationale = "Is a Gradle build dependency only and is not exposed in the release build. No Update available.",
+    )
 }
 
 cve("CVE-2023-6481") {
     snyk("SNYK-JAVA-CHQOSLOGBACK-6097492", v010)
+    suppress(
+        v010,
+        rationale = "Is a Gradle build dependency only and is not exposed in the release build. No Update available.",
+    )
 }
 
 cve("CVE-2023-6378") {
     snyk("SNYK-JAVA-CHQOSLOGBACK-6094943", v010)
+    suppress(
+        v010,
+        rationale = "Is a Gradle build dependency only and is not exposed in the release build. No Update available.",
+    )
 }
 
 cve("CVE-2023-6481") {
     snyk("SNYK-JAVA-CHQOSLOGBACK-6097493", v010)
+    suppress(
+        v010,
+        rationale = "Is a Gradle build dependency only and is not exposed in the release build. No Update available.",
+    )
 }
 
 cve("CVE-2020-29582") {
     snyk("SNYK-JAVA-ORGJETBRAINSKOTLIN-2393744", v010)
+    suppress(
+        v010,
+        rationale = "Is a Gradle build dependency only and is not exposed in the release build. No Update available.",
+    )
 }
