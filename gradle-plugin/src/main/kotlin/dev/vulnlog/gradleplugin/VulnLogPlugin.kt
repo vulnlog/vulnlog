828cb86cf18869179b12abe2198e4fae82482596
Pascal Knecht <pascal@addere.ch>
2025-02-21 13:28:32 +0100

    Fix Gradle plugin dependency group name

--- gradle-plugin/src/main/kotlin/dev/vulnlog/gradleplugin/VulnLogPlugin.kt
+++ gradle-plugin/src/main/kotlin/dev/vulnlog/gradleplugin/VulnLogPlugin.kt
@@ -7,7 +7,7 @@ import org.gradle.kotlin.dsl.get
 class VulnLogPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val dslVersion = javaClass.getResource("/version.txt")?.readText()?.lines()?.first() ?: ""
-        val dslDependency = project.dependencies.create("dev.vulnlog.dsl:dsl:$dslVersion")
+        val dslDependency = project.dependencies.create("dev.vulnlog:dsl:$dslVersion")
         project.configurations["compileOnly"].dependencies.add(dslDependency)
     }
 }
