4298985c70dfbe145e9326390891a8180969c700
Pascal Knecht <pascal@addere.ch>
2025-02-22 17:16:34 +0100

    Simplify serialisation by utilising Kotlin extension functions

--- cli/src/main/kotlin/dev/vulnlog/cli/commands/SerialisationTranslator.kt
+++ cli/src/main/kotlin/dev/vulnlog/cli/commands/SerialisationTranslator.kt
@@ -3,15 +3,30 @@ package dev.vulnlog.cli.commands
 import dev.vulnlog.cli.serialisable.ReleaseBranch
 import dev.vulnlog.cli.serialisable.ReleaseBranches
 import dev.vulnlog.cli.serialisable.ReleaseVersion
+import dev.vulnlog.dsl.ReleaseBranchData
+import dev.vulnlog.dsl.ReleaseVersionData
 
 class SerialisationTranslator {
     fun translate(filteredResult: Filtered): ReleaseBranches {
-        val releaseBranchToReleaseVersions =
-            filteredResult.releaseBranches.map { (releaseBranch, releaseVersions) ->
-                val rb = ReleaseBranch(releaseBranch.name)
-                val rVs = releaseVersions.map { rV -> ReleaseVersion(rV.version, rV.releaseDate) }
-                rb to rVs
+        return filteredResult.releaseBranches.toReleaseBranches()
+    }
+
+    private fun Map<ReleaseBranchData, List<ReleaseVersionData>>.toReleaseBranches(): ReleaseBranches {
+        val releaseBranchToReleaseVersions: Map<ReleaseBranch, List<ReleaseVersion>> =
+            map { (releaseBranch, releaseVersions) ->
+                releaseBranch.toReleaseBranch() to releaseVersions.map { it.toReleaseVersion() }
             }.toMap()
         return ReleaseBranches(releaseBranchToReleaseVersions)
     }
+
+    private fun ReleaseBranchData.toReleaseBranch() =
+        ReleaseBranch(
+            releaseBranchName = name,
+        )
+
+    private fun ReleaseVersionData.toReleaseVersion() =
+        ReleaseVersion(
+            version = version,
+            publicationDate = releaseDate,
+        )
 }
