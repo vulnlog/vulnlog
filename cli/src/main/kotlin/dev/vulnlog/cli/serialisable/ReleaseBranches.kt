a922a59d02d2c3002a4693121903ce457a80e992
Pascal Knecht <pascal@addere.ch>
2025-02-23 10:43:39 +0100

    Trim JSON structure

--- cli/src/main/kotlin/dev/vulnlog/cli/serialisable/ReleaseBranches.kt
+++
@@ -1,8 +0,0 @@
-package dev.vulnlog.cli.serialisable
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ReleaseBranches(
-    val releaseBranchesAndVersions: Map<ReleaseBranch, List<ReleaseVersion>>,
-)
