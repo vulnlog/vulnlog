998e6d8c8ff4e4b34d3ef7de450e784ec00ca522
Pascal Knecht <pascal@addere.ch>
2025-05-18 16:08:01 +0200

    Replace token variables in the reporter template with vulnerability information

--- suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionRecordTranslator.kt
+++ suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionRecordTranslator.kt
@@ -3,19 +3,21 @@ package dev.vulnlog.suppression
 import dev.vulnlog.dsl.VlReporterImpl
 import java.time.format.DateTimeFormatter
 
-class SuppressionRecordTranslator {
+class SuppressionRecordTranslator(private val tokenReplacer: SuppressionTokenReplacer) {
     private val dateFormater = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
     fun translate(vulnsPerBranchAndRecord: Set<VulnsPerBranchAndRecord>): Set<SuppressionRecord> {
         return vulnsPerBranchAndRecord.flatMap { (releaseBranch, reporter, vuln) ->
             val vulnTemplate = (reporter as VlReporterImpl).config?.template!!
             vuln.map { v ->
-                val suppression =
-                    vulnTemplate
-                        .replace("vulnlog-id", v.id)
-                        .replace("vulnlog-reasoning", v.analysisReasoning)
-                        .replace("vulnlog-start", v.suppressionStart.format(dateFormater))
-                v.suppressionEnd?.let { suppression.replace("vulnlog-end", it.format(dateFormater)) } ?: suppression
+                val replaceMap =
+                    mapOf(
+                        "vulnlogId" to v.id,
+                        "vulnlogReasoning" to v.analysisReasoning,
+                        "vulnlogStart" to v.suppressionStart.format(dateFormater),
+                        "vulnlogEnd" to v.suppressionEnd?.format(dateFormater),
+                    )
+                tokenReplacer.replaceTokens(SuppressionTokenData(vulnTemplate, replaceMap))
             }
                 .map { SuppressionRecord(releaseBranch, reporter, it) }
                 .toSet()
