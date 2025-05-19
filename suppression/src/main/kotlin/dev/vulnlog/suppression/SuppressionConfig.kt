a78867765c240d9eb1934e1117b614812473a750
Pascal Knecht <pascal@addere.ch>
2025-05-19 17:57:36 +0200

    Add support to write generated suppression files to STDOUT or to a file

--- suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionConfig.kt
+++ suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionConfig.kt
@@ -1,8 +1,5 @@
 package dev.vulnlog.suppression
 
-import java.io.File
-
 data class SuppressionConfig(
     val cliVersion: String,
-    val templateDir: File,
 )
--- suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionWriter.kt
+++ suppression/src/main/kotlin/dev/vulnlog/suppression/SuppressionWriter.kt
@@ -1,11 +1,88 @@
 package dev.vulnlog.suppression
 
-import java.io.File
+private const val MARKER_KEYWORD = "vulnlogEntries"
 
-@Suppress("unused")
-class SuppressionWriter(outputDir: File) {
-    @Suppress("unused")
-    fun writeSuppression(generatedSuppressions: Set<SuppressionRecord>) {
-        TODO("Not yet implemented")
+/**
+ * Handles the process of writing suppression data to output files or streams by combining provided templates
+ * with generated suppression records.
+ *
+ * The class utilizes a provided implementation of `OutputWriter` to write structured output data.
+ */
+class SuppressionWriter(private val outputWriter: OutputWriter) {
+    /**
+     * Writes suppression data into output files or streams by combining templates with generated suppression records.
+     *
+     * @param templateNameToContent a map where each key is an instance of `SuppressionFileInfo` representing a file's
+     * metadata (e.g., filename and extension) and the value is the content of that file as a list of strings.
+     * @param generatedSuppressions a set of `SuppressionRecord` instances representing the generated suppression data
+     * to be inserted into the templates.
+     */
+    fun writeSuppression(
+        templateNameToContent: Map<SuppressionFileInfo, List<String>>,
+        generatedSuppressions: Set<SuppressionRecord>,
+    ) {
+        insertSuppressionsIntoTemplate(generatedSuppressions, templateNameToContent)
+            .forEach(outputWriter::writeText)
     }
+
+    private fun insertSuppressionsIntoTemplate(
+        generatedSuppressions: Set<SuppressionRecord>,
+        templateNameToContent: Map<SuppressionFileInfo, List<String>>,
+    ): List<OutputData> =
+        generatedSuppressions.flatMap { record ->
+            val templateName = record.templateFilename
+            val templateEntry =
+                templateNameToContent.entries
+                    .firstOrNull { "${it.key.filename}.${it.key.fileExtension}" == templateName }
+            if (templateEntry == null) {
+                return emptyList()
+            }
+
+            val templateContent = templateEntry.value
+            val whitespaceCount = countWhiteSpacesBeforeMarker(templateContent)
+            val templateContentBeforeMarkerLine = extractContentBeforeMarkerLine(templateContent)
+            val templateContentAfterMarkerLine = extractContentAfterMarkerLine(templateContent)
+            createOutputData(
+                record,
+                templateEntry.key,
+                whitespaceCount,
+                templateContentBeforeMarkerLine,
+                templateContentAfterMarkerLine,
+            )
+        }
+
+    private fun countWhiteSpacesBeforeMarker(templateContent: List<String>) =
+        templateContent.first { it.contains(MARKER_KEYWORD) }.takeWhile { it.isWhitespace() }.length
+
+    private fun extractContentBeforeMarkerLine(templateContent: List<String>) =
+        templateContent.takeWhile { !it.contains(MARKER_KEYWORD) }
+
+    private fun extractContentAfterMarkerLine(templateContent: List<String>) =
+        templateContent.dropWhile { !it.contains(MARKER_KEYWORD) }.drop(1)
+
+    private fun createOutputData(
+        record: SuppressionRecord,
+        entry: SuppressionFileInfo,
+        whitespaceCount: Int,
+        beforeContent: List<String>,
+        afterContent: List<String>,
+    ) = record.branchToSuppressions.map { (branch, suppression) ->
+        val outputFileName = outputFilename(entry.filename, branch.name, entry.fileExtension)
+        val transformedContent =
+            suppression
+                .flatMap { it.split("\\n".toRegex()) }
+                .filterNot { it.isBlank() }
+                .map { " ".repeat(whitespaceCount) + it }
+        val content: List<String> = beforeContent + transformedContent + afterContent
+        OutputData(outputFileName, content)
+    }
+
+    private fun outputFilename(
+        reporterName: String,
+        branchName: String,
+        fileExtension: String,
+    ): String =
+        "$reporterName-$branchName.$fileExtension"
+            .replace("\\s+".toRegex(), "-")
+            .lowercase()
 }
