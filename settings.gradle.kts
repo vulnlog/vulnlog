59f1d28b1b568de6b572e9de92c363b844742e37
Pascal Knecht <pascal@addere.ch>
2025-10-03 18:13:09 +0200

    Move project files into new directory to create a mono repo
    this is a preparation commit to later add the website in this repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,27 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
 rootProject.name = "vulnlog"
 
+// Declare logical paths
 include(
-    "cli",
-    "common",
-    "dsl",
-    "dsl-interpreter",
-    "gradle-plugin",
-    "report",
-    "suppression",
+    ":cli",
+    ":common",
+    ":dsl",
+    ":dsl-interpreter",
+    ":gradle-plugin",
+    ":report",
+    ":suppression",
 )
+
+// Map them to their actual directories under cli-app/
+project(":cli").projectDir = file("cli-app/cli")
+project(":common").projectDir = file("cli-app/common")
+project(":dsl").projectDir = file("cli-app/dsl")
+project(":dsl-interpreter").projectDir = file("cli-app/dsl-interpreter")
+project(":gradle-plugin").projectDir = file("cli-app/gradle-plugin")
+project(":report").projectDir = file("cli-app/report")
+project(":suppression").projectDir = file("cli-app/suppression")
+
+//includeBuild("cli-app")
