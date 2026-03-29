2ea9f69521cc9fb5b7340b7aec91ff2a16a98a0e
Pascal Knecht <pascal@addere.ch>
2026-03-29 15:33:19 +0200

    refactor: Extract file parsing logic to shared function

--- next/cli-app/src/main/kotlin/dev/vulnlog/cli/result/ParseResult.kt
+++ next/cli-app/src/main/kotlin/dev/vulnlog/cli/result/ParseResult.kt
@@ -2,9 +2,17 @@ package dev.vulnlog.cli.result
 
 import dev.vulnlog.cli.model.ParseValidationVersion
 import dev.vulnlog.cli.model.VulnlogFile
+import java.io.File
 
 sealed interface ParseResult {
     data class Ok(val validationVersion: ParseValidationVersion, val content: VulnlogFile) : ParseResult
 
     data class Error(val error: String) : ParseResult
+}
+
+data class ParseResults(
+    val success: Map<File, ParseResult.Ok> = emptyMap(),
+    val failure: Map<File, ParseResult.Error> = emptyMap(),
+) {
+    fun onEachFailure(action: (File, ParseResult.Error) -> Unit) = failure.forEach(action)
 }
