3463c89a71959ff715f0eddb02845a4680b3ac67
Pascal Knecht <pascal@addere.ch>
2026-03-29 16:24:59 +0200

    refactor: Extract validation logic into shared function

--- next/cli-app/src/main/kotlin/dev/vulnlog/cli/shell/ValidateCommand.kt
+++ next/cli-app/src/main/kotlin/dev/vulnlog/cli/shell/ValidateCommand.kt
@@ -9,11 +9,8 @@ import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.path
-import dev.vulnlog.cli.core.renderValidation
-import dev.vulnlog.cli.core.validate
-import dev.vulnlog.cli.model.VulnlogFileContext
-import dev.vulnlog.cli.result.ValidationResult
 import dev.vulnlog.cli.shell.shared.parseFiles
+import dev.vulnlog.cli.shell.shared.validateFiles
 import java.nio.file.Path
 
 class ValidateCommand : CliktCommand(name = "validate") {
@@ -42,32 +39,14 @@ class ValidateCommand : CliktCommand(name = "validate") {
             throw ProgramResult(ExitCode.VALIDATION_ERROR.ordinal)
         }
 
-        // validate and check for validation findings
-        val validationFindings: Map<VulnlogFileContext, ValidationResult> =
-            parseResults.success
-                .map { (file, parseResult) ->
-                    VulnlogFileContext(
-                        parseResult.validationVersion,
-                        file.name,
-                        parseResult.content,
-                    )
-                }
-                .associateWith { context -> validate(context) }
-        if (validationFindings.values.any { it.findings.isNotEmpty() }) {
-            val result =
-                validationFindings
-                    .filter { (_, findings) -> findings.findings.isNotEmpty() }
-                    .map { (context, findings) -> context.fileName to renderValidation(findings) }
-                    .joinToString("\n\n") { (filename, results) -> "Validation findings for $filename:\n$results" }
-            echo(result, err = true)
-
-            val hasAnyError: Boolean = validationFindings.values.any { it.errors.isNotEmpty() }
-            val hasAnyWarning: Boolean = validationFindings.values.any { it.warnings.isNotEmpty() }
-            if (hasAnyError || (hasAnyWarning && strict)) {
-                throw ProgramResult(ExitCode.VALIDATION_ERROR.ordinal)
-            }
+        val validationFindings = validateFiles(parseResults.success)
+        if (validationFindings.renderedFindings.isNotBlank()) {
+            echo(validationFindings.renderedFindings, err = true)
         }
-
-        echo("Validation OK")
+        if (validationFindings.hasErrors || (validationFindings.hasWarnings && strict)) {
+            throw ProgramResult(ExitCode.VALIDATION_ERROR.ordinal)
+        } else {
+            echo("Validation OK")
+        }
     }
 }
---
+++ next/cli-app/src/main/kotlin/dev/vulnlog/cli/shell/shared/ValidateFile.kt
@@ -0,0 +1,35 @@
+package dev.vulnlog.cli.shell.shared
+
+import dev.vulnlog.cli.core.renderValidation
+import dev.vulnlog.cli.core.validate
+import dev.vulnlog.cli.model.VulnlogFileContext
+import dev.vulnlog.cli.result.ParseResult
+import dev.vulnlog.cli.result.ValidationResult
+import dev.vulnlog.cli.result.ValidationResults
+import java.io.File
+
+fun validateFiles(fileToResult: Map<File, ParseResult.Ok>): ValidationResults {
+    val contextToResults = validateEachFile(fileToResult)
+    val renderedFindings =
+        contextToResults
+            .filter { (_, findings) -> findings.findings.isNotEmpty() }
+            .map { (context, findings) -> context.fileName to renderValidation(findings) }
+            .joinToString("\n\n") { (filename, results) -> "Validation findings for $filename:\n$results" }
+    return ValidationResults(
+        renderedFindings = renderedFindings,
+        hasErrors = contextToResults.values.any { it.errors.isNotEmpty() },
+        hasWarnings = contextToResults.values.any { it.warnings.isNotEmpty() },
+    )
+}
+
+private fun validateEachFile(fileToResult: Map<File, ParseResult.Ok>): Map<VulnlogFileContext, ValidationResult> {
+    return fileToResult
+        .map { (file, parseResult) ->
+            VulnlogFileContext(
+                parseResult.validationVersion,
+                file.name,
+                parseResult.content,
+            )
+        }
+        .associateWith { context -> validate(context) }
+}
