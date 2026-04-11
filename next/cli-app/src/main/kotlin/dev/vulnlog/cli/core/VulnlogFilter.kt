9fe2d54a5b80218feae71f87886a8e6da9052be4
Pascal Knecht <pascal@addere.ch>
2026-04-11 11:56:04 +0200

    fix: Create suppressions for each reporter-specific vulnerability ID (#88)
    Use vuln_ids values from the report entry for the target reporter type, if present. Otherwise use the vulnerability's id field.
    
    aliases are not used for suppression.

--- next/cli-app/src/main/kotlin/dev/vulnlog/cli/core/VulnlogFilter.kt
+++ next/cli-app/src/main/kotlin/dev/vulnlog/cli/core/VulnlogFilter.kt
@@ -4,6 +4,9 @@ import dev.vulnlog.cli.model.Release
 import dev.vulnlog.cli.model.ReporterType
 import dev.vulnlog.cli.model.Tag
 import dev.vulnlog.cli.model.VulnerabilityEntry
+import dev.vulnlog.cli.model.suppress.SuppressedVulnerability
+import java.time.LocalDate
+import kotlin.sequences.filter
 
 data class VulnlogFilter(
     val releases: Set<Release> = emptySet(),
@@ -11,7 +14,17 @@ data class VulnlogFilter(
     val reporter: ReporterType? = null,
 )
 
+data class SuppressionFilter(
+    val filter: VulnlogFilter = VulnlogFilter(),
+    val today: LocalDate = LocalDate.now(),
+)
+
 fun Sequence<VulnerabilityEntry>.applyFilter(filter: VulnlogFilter): Sequence<VulnerabilityEntry> =
     this
         .filter { filter.releases.isEmpty() || it.releases.any { release -> release in filter.releases } }
         .filter { filter.tags.isEmpty() || filter.tags.any { tag -> it.tags.contains(tag) } }
+
+fun Sequence<SuppressedVulnerability>.applyFilter(filter: SuppressionFilter): Sequence<SuppressedVulnerability> =
+    this
+        .filter { filter.filter.releases.isEmpty() || it.releases.any { release -> release in filter.filter.releases } }
+        .filter { filter.filter.tags.isEmpty() || filter.filter.tags.any { tag -> it.tags.contains(tag) } }
