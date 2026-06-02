19cd23b88a3ace6639217f9376a150db165e5586
Pascal Knecht <pascal@addere.ch>
2026-06-02 13:41:39 +0200

    feat: Add missing Jackson annotations in DTOs for non-null values (#159)
    This is a preparation for the following format command.
    
    Signed-off-by: Pascal Knecht <pascal@addere.ch>

--- modules/lib/src/main/kotlin/dev/vulnlog/lib/parse/v1/dto/ReportEntryDto.kt
+++ modules/lib/src/main/kotlin/dev/vulnlog/lib/parse/v1/dto/ReportEntryDto.kt
@@ -11,6 +11,7 @@ import java.time.LocalDate
 data class ReportEntryDto(
     val reporter: String,
     @param:JsonFormat(pattern = "yyyy-MM-dd")
+    @param:JsonInclude(JsonInclude.Include.NON_NULL)
     val at: LocalDate? = null,
     @param:JsonInclude(JsonInclude.Include.NON_NULL)
     val source: String? = null,
